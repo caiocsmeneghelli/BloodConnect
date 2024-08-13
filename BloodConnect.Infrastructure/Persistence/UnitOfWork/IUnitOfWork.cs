53a7eec89f5d2ba0c5ce92dda267bd222d3da542
Caio Cesar <caiocsmeneghelli@outlook.com>
2024-08-12 21:16:12 -0300

    Add unit of work.

---
+++ BloodConnect.Infrastructure/Persistence/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using BloodConnect.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodConnect.Infrastructure.Persistence.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IDonorRepository Donors { get; }
+        IDonationRepository Donations { get; }
+        IAddressRepository Addresses { get; }
+        IBloodStockRepository BloodStocks { get; }
+
+        Task<int> CompletAsync();
+        Task BeginTransactionAsync();
+        Task CommitAsync();
+    }
+}
---
+++ BloodConnect.Infrastructure/Persistence/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,72 @@
+﻿using BloodConnect.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodConnect.Infrastructure.Persistence.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private IDbContextTransaction _transaction;
+        private readonly BloodConnectContext _context;
+
+        public UnitOfWork(IDonorRepository donors, IDonationRepository donations,
+            IAddressRepository addresses, IBloodStockRepository bloodStocks, BloodConnectContext context)
+        {
+            Donors = donors;
+            Donations = donations;
+            Addresses = addresses;
+            BloodStocks = bloodStocks;
+            _context = context;
+        }
+
+        public IDonorRepository Donors { get; }
+
+        public IDonationRepository Donations {get;}
+
+        public IAddressRepository Addresses {get;}
+
+        public IBloodStockRepository BloodStocks {get;}
+
+        public async Task BeginTransactionAsync()
+        {
+            _transaction = await _context.Database.BeginTransactionAsync();
+        }
+
+        public async Task CommitAsync()
+        {
+            try
+            {
+                await _transaction.CommitAsync();
+            }
+            catch (Exception ex)
+            {
+                await _transaction.RollbackAsync();
+                throw ex;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+        }
+
+        public async Task<int> CompletAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
